55445c860d9f83f8734d22442e918153a25aa947
yeonghaIm <dudgk6382@gmail.com>
2021-10-27 18:34:06 +0900

    add3

--- refactoring3/src/db/index.ts
+++ refactoring3/src/db/index.ts
@@ -2,6 +2,7 @@ import faunadb from 'faunadb';
 
 const secretKey = 'fnAEWh1OJzAAxp6DCscO17KkYU2bHnnmsAQoo9v5';
 
-const serverClient = new faunadb.Client({ secret: secretKey });
+const serverClient = new faunadb.Client({ secret: (process.env as any).KEY
+});
 
 export default serverClient;
